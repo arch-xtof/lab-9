c9486e77791a381089e5859804d3cbce4775ed1c
Archil Kristinashvili <arkris@ttu.ee>
2020-04-22 02:41:20 +0300

    wtf

--- index.php
+++ index.php
@@ -57,13 +57,13 @@
         }
         printf("</ul>");
 
-        printf("<form method=POST action=index.php> <input name=search> <input type=submit> </form>");
+        printf("<form method=POST> <input name=search> <input type=submit> </form>");
 
         if(isset($_GET['semester'])){
             $semester = mysqli_real_escape_string($db, $_GET['semester']);
         }
         
-        $search = '';
+        $search = 'yo';
         if(isset($POST['search'])){
             $search = $_POST['search'];
         }
