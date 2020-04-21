eb4a2639b5d6c543ad73f8ce1c464ccb81f1548e
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 23:35:02 +0300

    davigale 2

--- test.php
+++ test.php
@@ -2,4 +2,10 @@
 
 echo "gay";
 
+include_once "connect.db.php";
+
+// Create connection
+//$db = mysqli_connect($server, $user, $password, $database);
+$db = mysqli_connect("anysql.itcollege.ee", "WT8", "CJuPlun24D", "WT8");
+
 ?>
