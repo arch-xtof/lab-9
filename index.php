1f0372780be8528cab60599742d6ddb0892125e2
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 22:39:25 +0300

    fix test

--- index.php
+++ index.php
@@ -10,15 +10,19 @@
         die("Connection failed: " . $db->connect_error);
     }
     //echo "Connected successfully";
-
+    echo '4';
     function listCourses(){
+        echo '3';
         $query = "SELECT course_code, course_name, ects_credits FROM courses ORDER BY id ASC";
         $result = mysqli_query($db, $query);
         if(mysqli_num_rows($result) > 0){
             
             while($row = mysqli_fetch_array($result)){
                 printf("%s\t %s\t %s", $row['course_code'], $row['course_name'], $row['ects_credits']);
+                echo '1';
             }
+
+            echo '2';
         }
     }
 
