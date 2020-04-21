ce93f0ddea2597bf4c74173b4528616064af8ac1
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 23:49:24 +0300

    sdasa

--- index.php
+++ index.php
@@ -17,25 +17,24 @@
         echo "weird";
     }
 
-    if(mysqli_num_rows($result) > 0){
+    /*if(mysqli_num_rows($result) > 0){
         echo "1";
         while($row = mysqli_fetch_array($result)){
             printf("%s\t %s\t %s", $row['course_code'], $row['course_name'], $row['ects_credits']);
         }
-    }
+    }*/
 
-    /*function listCourses(){
+    function listCourses(){
         
         $query = "SELECT * FROM courses";
         $result = mysqli_query($db, $query);
-        echo "2";
         if(mysqli_num_rows($result) > 0){
-            echo "1";
+            
             while($row = mysqli_fetch_array($result)){
                 printf("%s\t %s\t %s", $row['course_code'], $row['course_name'], $row['ects_credits']);
             }
         }
     }
     
-    //listCourses();*/
+    listCourses();
 ?>
