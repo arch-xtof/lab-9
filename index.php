97af93983c89521b9d40fd0aab3531d3dc170cc9
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 22:54:51 +0300

    fixes

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         echo '3';
         $query = "SELECT course_code, course_name, ects_credits FROM courses ORDER BY id ASC";
         $result = mysqli_query($db, $query);
+        echo mysqli_num_rows($result);
         if(mysqli_num_rows($result) > 0){
             
             while($row = mysqli_fetch_array($result)){
