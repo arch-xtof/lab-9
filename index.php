d5eb2bf41be304a476dde86feb2ce8a8dadc2d30
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 23:13:11 +0300

    maybe this time

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
     // Get credentials
-    require('connect.db.php');
+    include_once "connect.db.php";
 
     // Create connection
-    $db = mysqli_connect($server, $user, $password, $database);
-
+    //$db = mysqli_connect($server, $user, $password, $database);
+    $db = mysqli_connect("anysql.itcollege.ee", "WT8", "CJuPlun24D", "WT8");
     // Check connection
     if(!$db) die("Connnection to DB failed: " . mysqli_connect_error());
 
     function listCourses(){
         
-        $query = "SELECT * FROM courses ;";
+        $query = "SELECT * FROM courses;";
         $result = mysqli_query($db, $query);
-        
+        echo "2";
         if(mysqli_num_rows($result) > 0){
-            
+            echo "1";
             while($row = mysqli_fetch_array($result)){
                 printf("%s\t %s\t %s", $row['course_code'], $row['course_name'], $row['ects_credits']);
             }
