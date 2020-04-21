b6065c90ecca4a6e8ca26729071d538494aec00b
Archil Kristinashvili <arkris@ttu.ee>
2020-04-21 23:35:50 +0300

    12

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php 
 
-echo "gay";
-
 include_once "connect.db.php";
 
 // Create connection
 //$db = mysqli_connect($server, $user, $password, $database);
 $db = mysqli_connect("anysql.itcollege.ee", "WT8", "CJuPlun24D", "WT8");
+
+if(!$db) die("Connnection to DB failed: " . mysqli_connect_error());
+echo "bruh";
 
 ?>
